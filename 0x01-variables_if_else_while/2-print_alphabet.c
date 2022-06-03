7681fdb8395592700ef44561e046292e8f87fc49
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-03 04:48:54 -0700

    Alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -12,6 +12,6 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	        printf("\n");
+		printf("\n");
 	return(0);
 }
