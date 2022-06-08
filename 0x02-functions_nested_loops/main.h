d3d99154a64ca93f058ac6889a9453338390f43d
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-07 17:21:04 -0700

    main h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifindef _ALX_H_
+#define _ALX_H_
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_dogot(int);
+void 24_hours(void);
+int times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif /* _ALX_H_ */
