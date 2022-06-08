6425f3923ca7eba839cc1d8bc9b9c7f77cd72883
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-07 21:16:22 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 			}
 			else if (product >= 10)
 			{
-				_putchar((product / 10) +'0')
+				_putchar((product / 10) +'0');
 				_putchar((product % 10) + '0');
 			}
 			else if ((product < 10) && (y != 0))
