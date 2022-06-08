75b6e7ec069d6a314ce89301d50c248ab126c208
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-07 18:52:46 -0700

    alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: single letter input
  * Return: 1 if c is a letter, lowercase or uppercase, O if otherwise
  */
-int_isalpha(int c)
+int _isalpha(int c)
 {
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
