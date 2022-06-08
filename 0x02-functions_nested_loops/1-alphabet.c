0584bec198099f18eaff4c40d2ab157a155041c6
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-07 18:18:27 -0700

    Lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Alawys 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
