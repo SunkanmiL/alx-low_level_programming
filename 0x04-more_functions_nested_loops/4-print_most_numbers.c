51e5afe534693bb70490a3c8e55cb974e6b0bdf5
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-10 04:42:58 -0700

    numberz

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,10 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i);
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+			else
+			_putchar(i + '0');
+	}
 	_putchar('\n');
 }
 
