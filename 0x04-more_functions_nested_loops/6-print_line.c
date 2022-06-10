bb1d56c39dbb83e4da78f3ef4c9c3e9b172e4d25
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-10 05:11:58 -0700

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #inlcude "main.h"
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character "_" should be printed
+ * @n: number of times the character '_' should be printed
  * Return: Always 0 (Success)
  */
 void print_line(int n)
