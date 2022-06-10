39f4918c498bc6f22940fa1bc40566e84e9f569a
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-10 05:19:22 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,10 @@
  */
 void print_line(int n)
 {
-	int co;
+	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (co = 1; co <= n; co++)
-		{
+	if (n > 0)
+		for (i = 0; i < n; i++)
 			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
