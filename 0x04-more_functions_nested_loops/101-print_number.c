431c2d3cc4b66323c6f1b8e4d1bb0e17393335f2
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-10 08:37:39 -0700

    integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,5 +6,33 @@
  */
 void print_number(int n)
 {
-	return (0);
+	long len, res, i, temp, expo;
+
+	res = n;
+	expo = len = 1;
+/*check negatives*/
+	if (res < 0)
+	{
+		res *= -1;
+		_putchar('-');
+	}
+
+	temp = res;
+	while (temp >= 10)
+	{
+		len++;
+		temp /= 10;
+	}
+
+/*create exponent*/
+	for (i = 1; i < len; i++)
+		expo *= 10;
+
+/*main*/
+	while (expo > 1)
+	{
+		_putchar((res / expo) % 10 + '0');
+		expo /= 10;
+	}
+	_putchar(res % 10 + '0');
 }
