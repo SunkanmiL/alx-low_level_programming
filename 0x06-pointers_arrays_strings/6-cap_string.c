e532c4b7a22aa214761ef4d19bef4703c608d421
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-15 14:12:24 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * *cap_string - function that capitalizes all words of a string
- * @n: input string
+ * @targ: input string
  * Return: string with capitalized words
  */
 char *cap_string(char *targ)
