7540d24c9cf76fc3cad46137aef7d75bba7bb92d
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-15 05:01:30 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *strncpy - function that copies a string
+ * @src: copy from
+ * @dest: copy to
+ * @n: number of bytes
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
