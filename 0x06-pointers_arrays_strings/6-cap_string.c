d5a27c54b6fcc6d8ff12dd6345f24bb753e13c01
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-15 13:33:29 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@ char *cap_string(char *n)
 {
 	int i, x;
 	int cap = 32;
-	int separators[] = {',', ';', '.', '!', '?', '"', '(', ')', '{' '}',
+	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{' '}',
 		' ', '\n', '\t'};
 
 	for (i = 0; n[i] != '\0'; i++)
