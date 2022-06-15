cdefc547e381fe3b7157189c18f25b32769dbe41
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-15 11:24:54 -0700

    comapre

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-	for (s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
