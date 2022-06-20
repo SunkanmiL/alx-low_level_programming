830f11fe719076b4e53905521620ac2472ef0502
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-20 12:51:38 -0700

    searches

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,13 +10,13 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, n;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (n = 0; accept[n]; n++)
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (s[i] == accept [n])
+			if (s[i] == accept[n])
 				return (s + i);
 		}
 	}
-	return ('\0');
+	return (NULL);
 }
