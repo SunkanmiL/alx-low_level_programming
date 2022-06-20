ee18970430e94e4de93e99c5159f591ae5d6b9dc
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-20 09:48:16 -0700

    copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,14 +5,14 @@
  * @src: source
  * @dest: destination
  * @n: number of bytes to copy
- * Return: a pointer to destination
+ * Return: a pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	while (n > 0)
 	{		
-		dest[n - 1] = src [n - 1];
+		dest[n - 1] = src[n - 1];
 		n--;
 	}
 	return (dest);
