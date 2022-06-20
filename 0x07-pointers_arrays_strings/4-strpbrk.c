8682639c55ba9332d96844829f92cc3bec07ff9f
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-20 10:56:12 -0700

    search string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strpbrk - function that searches a string for any of a set of bytes
+ * *_strpbrk - function that searches a string for any of a set of bytes
  * @s: string being tested
  * @accept: input
  * Return: Always 0 (Success)
@@ -9,15 +9,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, n;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (s[i] == accept[n])
+			if (s[i] == accept [n])
 				return (s + i);
 		}
 	}
-
 	return (NULL);
 }
