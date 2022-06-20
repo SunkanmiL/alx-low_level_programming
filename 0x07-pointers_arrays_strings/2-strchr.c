f88a6e998f0b9afbd4a3cf19c5d705c4a639ad75
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-20 10:16:12 -0700

    locates

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *null = NULL;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
 	}
-	if (s[i] == '\0')
-		return (s + i);
-	else
-		return (NULL);
+
+	return (NULL);
 }
