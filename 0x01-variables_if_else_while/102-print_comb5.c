313f0a824b6b5b5e37344f1758032c9adcf9cb2c
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-21 01:07:21 -0700

    combinations5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 98; x++)
+	for (x = 0; x <= 99; x++)
 	{
-		for (y = x + 1; y <= 98; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
 			putchar((x / 10) + '0');
 			putchar((x % 10) + '0');
@@ -18,7 +18,7 @@ int main(void)
 			putchar((y / 10) + '0');
 			putchar((y % 10) + '0');
 
-			if (x == 98 && y == 99)
+			if (x == 99 && y == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
