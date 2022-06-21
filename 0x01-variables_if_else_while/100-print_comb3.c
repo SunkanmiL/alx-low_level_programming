7836127402b055a8fa3bf30ddde026689a602b77
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-20 23:56:21 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,14 @@ int main(void)
 	int d;
 
 	for (d = 0; d < 90; d++)
+	{	
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+	}
+	if (d != 89)
 	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
