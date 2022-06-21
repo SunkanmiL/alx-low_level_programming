b31ebd2dceaeb3a810900d1b911c8766340837ee
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-21 00:49:02 -0700

    combinations4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			for (z = 0; z < 10; z++)
+			{
+				if (y > x && z > y)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+
+					if (x != 7 || y != 8 || z != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
