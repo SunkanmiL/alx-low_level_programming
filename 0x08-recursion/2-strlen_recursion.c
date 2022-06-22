19f701c33bd476d2fd17889ffef060f16fa18aa2
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-22 12:49:02 -0700

    returns length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s == '\0')
 		return (0);
-	if (s[1] == '\0')
-		return (1);
 	else
-		return (_strlen_recursion(&s[1]) + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
