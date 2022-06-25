ea24be0e79cebcf71c353420764f87c48bbdf24b
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-24 21:51:01 -0700

    adds positive

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,22 +8,23 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
-	int i, sum = 0;
+	int num, result = 0, i;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	while (argc-- > 1)
 	{
-		if (!atoi(argv[i]))
+		for (i = 0; argv[argc][i]; i++)
 		{
-			printf("%s\n", "Error");
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
 			return (1);
+			}
 		}
-		sum += atoi(argv[i]);
+		num = atoi(argv[argc]);
+		result += num;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", result);
 	return (0);
 }
