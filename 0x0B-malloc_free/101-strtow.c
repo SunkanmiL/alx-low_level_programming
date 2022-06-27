d3b6d333c512aedadc2bef3475871875fed17802
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-27 03:15:09 -0700

    splits string

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ char **strtow(char *str)
 		return (NULL);
 	for (; str[i]; i++)
 	{
-		if ((str[i] != ' ' || *str != '\t') && 
+		if ((str[i] != ' ' || *str != '\t') &&
 				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
 			count++;
 	}
