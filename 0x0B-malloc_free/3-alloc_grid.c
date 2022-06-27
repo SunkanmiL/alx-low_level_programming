9640743c1f9382a8ffeee731b3d9cd42225c685c
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-27 01:47:48 -0700

    2dim array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 /**
  * **alloc_grid - returns a pointer to a 2D array of integers
- * @grid: the address of the two dimensional grid
  * @width: width of the grid
  * @height: height of the grid
  * Return: pointer to a 2D array, NULL on failure
