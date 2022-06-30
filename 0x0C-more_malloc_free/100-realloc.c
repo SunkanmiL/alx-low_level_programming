2687c2302af6598fdb5e70e4d3cc9231da996f16
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-30 14:48:56 -0700

    reallocates memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 
-void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *dest, *src;
 	unsigned int i;
