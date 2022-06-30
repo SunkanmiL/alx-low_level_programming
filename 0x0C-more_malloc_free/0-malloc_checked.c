e1889986093f2a375f1f2594419efba742159c46
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-06-30 12:10:39 -0700

    alloccates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 /**
  * malloc_checked - function that allocates memory using malloc
  * @b: variable to allocate memory for 
