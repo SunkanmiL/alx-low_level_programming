611b7bde318f0ca92803d72224a3d7313ecbc7fd
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-07-04 19:53:53 -0700

    create new-dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strdup - function that returns a pointer to a newly allocated space memory
+ * @str: string
+ * Return: pointer of duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	ptr = (char *)malloc(sizeof(char) * (i + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
+
+/**
  * *new_dog - function that creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -12,36 +41,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_name;
-	char *copy_name, *copy_owner;
-	int i, len_name = 0, len_owner = 0;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
 
 	new_name = malloc(sizeof(dog_t));
 	if (new_name == NULL)
 		return (NULL);
-
-	copy_name = malloc(len_name + 1);
-	if (copy_name == NULL)
+	(*new_name).name = _strdup(name);
+	if ((*new_name).name == NULL)
+	{
+		free(new_name);
 		return (NULL);
-	for (i = 0; name[i]; i++)
-		copy_name[i] = name[i];
-	copy_name[i] = '\0';
-
-	copy_owner = malloc(len_owner + 1);
-	if (copy_owner == NULL)
+	}
+	(*new_name).age = age;
+	(*new_name).owner = _strdup(owner);
+	if ((*new_name).owner == NULL)
+	{
+		free((*new_name).name);
+		free(new_name);
 		return (NULL);
-	for (i = 0; owner[i]; i++)
-		copy_owner[i] = owner[i];
-	copy_owner[i] = '\0';
-
-	new_name->name = copy_name;
-	new_name->age = age;
-	new_name->owner = copy_owner;
+	}
 	return (new_name);
 }
