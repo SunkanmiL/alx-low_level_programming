16f6a4a4ffe8f71d58b6c3c759d8b66f0b1c1400
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-07-04 18:59:38 -0700

    create new-dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,83 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-int get_len(int i);
-char *str_cpy(char *dest, char *src);
+
+char *_strdup(char *str);
+
 /**
  * new_dog - function that creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * Return: Always 0 (Success)
+ * Return: a pointer to the new dog or NULL if failed
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *copy_name, *copy_owner;
-	unsigned int i, len_name = 0, len_owner = 0;
+	dog_t *new_dog = NULL;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (name == NULL)
-		return (NULL);
-	if (name == NULL || age <= 0 || owner == NULL)
+	if (name == NULL || owner == NULL)
 	{
+		return (NULL);
+	}
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = _strdup(name);
+
+	if (new_dog-> == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	for (i = 0; name[i] != '\0'; i++)
-		len_name++;
-	for (i = 0; owner[i] != '\0'; i++)
-		len_owner++;
-
-	copy_name = malloc(sizeof(char) * (len_name + 1));
-	if (copy_name == NULL)
-		return (NULL);
-	copy_owner = malloc(sizeof(char) * (len_owner + 1));
-	if (copy_owner == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len_name; i++)
-		copy_name[i] = name[i];
-	for (i = 0; i <= len_owner; i++)
-		copy_owner[i] = owner[i];
-
-	new_dog->name = copy_name;
-	new_dog->owner = copy_owner;
-	new_dog->age = age;
 	return (new_dog);
+}
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space memory
+ * @str: string
+ * Return: the pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	while (str(len) != '\0')
+	{
+		len++;
+	}
+
+	ptr = malloc(sizeof(char) * len + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	while (str[len] != '\0')
+	{
+		ptr[len] = str[len];
+		len++;
+	}
+	ptr[len] = '\0';
+	return (ptr);
 }
