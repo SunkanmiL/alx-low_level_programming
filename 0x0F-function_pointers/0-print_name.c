8c949525ea57cc2f513f45aee5cabb4a5f7b8417
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-07-06 03:29:08 -0700

    a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - function that prints a name
  * @name: name of the person
  * @f: function pointer
- * Return: Always 0 (Success)
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
