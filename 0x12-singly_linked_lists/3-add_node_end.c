9db0484609311e9d3af44a68a6cb2e37cba9baf6
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-07-14 09:39:11 -0700

    add node-at-the-end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -36,6 +36,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (temp->next != NULL)
 		temp = temp->next;
 
-	temp->next = new;
-	return (new);
+	temp->next = new_node;
+	return (new_node);
 }
