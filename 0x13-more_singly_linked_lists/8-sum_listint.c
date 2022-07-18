598cb0798ad6c83f1b5244677e1ed4a858a35a2a
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-07-18 13:59:06 -0700

    sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_listint - function that returns the sum of all the data in the linked list
+ * sum_listint - function that return d sum of all the data in the linked list
  * @head: pointer to the head node of the linked list
  * Return: Sum of all the data, or 0 if the list is empty
  */
