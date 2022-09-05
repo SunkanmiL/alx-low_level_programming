bb516206ac261d39513efb7b8fa83c3d7429a808
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-05 11:34:18 -0700

    copy memory

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_memcpy - function that copies memory area
+ * @src: source
+ * @dest: destination
+ * @n: number of bytes to copy
+ * Return: a pointer to destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n > 0)
+	{
+		dest[n - 1] = src[n - 1];
+		n--;
+	}
+	return (dest);
+}
