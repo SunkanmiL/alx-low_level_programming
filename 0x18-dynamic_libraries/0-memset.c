6bc7c61baac6ca317d24b2979c9f4aa1b0f1c285
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-05 11:22:50 -0700

    Dynamic library

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @n: bytes
+ * @s: pointed destination
+ * @b: constant byte
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
