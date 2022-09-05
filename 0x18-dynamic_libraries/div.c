a6271fcb9e685840f8c8dc75f2d44b52a2c5019d
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-05 13:02:34 -0700

    division

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include "pymain.h"
+/**
+ * div - divides two digits
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: division of two arguments
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
